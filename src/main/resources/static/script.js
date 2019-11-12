490cd21d240ec8a2c42fdbc357daff1a80f7c856
JupiterPi <jupiterpi@outlook.de>
2019-11-12 20:41:48 +0100

    showTables() with failures

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,6 +3,7 @@ function getJSONAsTable(url, divId, additional) {
     getJSON(url, function(json) {
         writeTable(divId, json);
         metalizeTable("tablein:" + divId);
+        console.log("addidtionals doing now!!!!!!");
         additional();
     })
 }
@@ -12,6 +13,7 @@ function addTableLink(divId, imgSrc, action) {
     var link = document.createElement("a");
     link.href="#";
     link.onclick = action;
+    console.log("action:::" + action);
 
     var img = document.createElement("img");
     img.src = imgSrc;
@@ -49,4 +51,29 @@ function showTransactions(accountId) {
     div.appendChild(heading);
 
     getJSONAsTable("/transaction/" + accountId, "transactions_table", function(){});
+}
+
+function showTable(name, mapping, id, linkingFunction, imgSrc) {
+    var divId = name + "s_table";
+    console.log(divId);
+    var div = element(divId);
+    var heading = document.createElement("h2");
+    heading.innerText = makeUpperCase(name) + "s of " + id;
+    div.innerHTML = "";
+    div.appendChild(heading);
+
+    var linking = function() {
+        console.log("linking dig mit test: " + name);
+        var action = function() {
+            console.log("action did");
+            linkingFunction(this.parentNode.parentNode.childNodes[0].innerText);
+        }
+        addTableLink(name + "s_table", imgSrc, action);
+    }
+
+    getJSONAsTable(mapping + "/" + id, divId, linking);
+}
+
+function makeUpperCase(str) {
+    return str[0].toUpperCase() + str.slice(1);
 }
